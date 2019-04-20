139ca0c2a83780aaf2e17f9184293c17d5be1499
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-04-20 16:00:38 +0000

    reddit http 2

--- http-reddit/src/app/app.component.ts
+++ http-reddit/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Article } from './article/article.model';
 
 @Component({
@@ -7,6 +9,66 @@ import { Article } from './article/article.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+ articles:Article[];   // <-- component property
+ data: Object;
+   loading: boolean;
+   o :Observable<Article[]>;
+
+
+  constructor(public http: HttpClient){
+    this.makeTypedRequest();
+    /*this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];*/
+  }
+
+  makeTypedRequest(): void {
+    this.o = this.http.get<Article[]>('https://jsonplaceholder.typicode.com/posts');
+    this.o.subscribe(data => {this.articles = data;});
+
+   }
+   //Nota bene, questo è un metodo alternativo e compatto per fare la stessa cosa di
+   //makeRequest senza dichiarare la variabile Observable e creando l’arrow function
+   //direttamente dentro il metodo subscribe
+   makeCompactPost(): void {
+   this.loading = true;
+   this.http
+     .post('https://jsonplaceholder.typicode.com/posts',
+       JSON.stringify({
+         body: 'bar',
+         title: 'foo',
+         userId: 1
+       })
+     )
+     .subscribe(data => {
+       this.data = data;
+       this.loading = false;
+     });
+ }
+
+
+
+  addArticle(userId: HTMLInputElement, id: HTMLInputElement, title: HTMLInputElement, body: HTMLInputElement): boolean {
+    //console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article( Number(userId.value), Number(id.value), title.value, body.value));
+    title.value = '';
+    userId.value = '';
+    body.value = '';
+    id.value = '';
+    return false;
+  }
+
+  /*sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }*/
+
+  
+
+
   /*addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     let t = title.value;
     let v= link.value;
@@ -15,26 +77,5 @@ export class AppComponent {
     //console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
   }*/
- 
- articles:Article[];   // <-- component property
-  constructor(){
-    this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1),
-    ];
-  }
-
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    this.articles.push(new Article(title.value, link.value, 0));
-    title.value = '';
-    link.value = '';
-    return false;
-  }
-  
-  sortedArticles(): Article[] {
-    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
-  }
 
 }
--- http-reddit/src/app/article/article.model.ts
+++ http-reddit/src/app/article/article.model.ts
@@ -1,14 +1,18 @@
 export class Article {
+  userId: number;
+  id: number;
   title: string;
-  link: string;
+  body: string;
   votes: number;
 
-  constructor(title: string, link: string, votes: number) {
+  constructor(userId: number, id: number, title: string, body: string) {
+    this.userId = userId;
+    this.id = id;
     this.title = title;
-    this.link = link;
-    this.votes = votes || 0;
+    this.body = body;
+    this.votes = 0;
   }
-  
+
   voteUp(): void {
     this.votes += 1;
   }
@@ -19,15 +23,15 @@ export class Article {
 
   // domain() is a utility function that extracts
   // the domain from a URL, which we'll explain shortly
-  domain(): string {
+  /*domain(): string {
     try {// e.g. http://foo.com/path/to/bar
      const domainAndPath: string = this.link.split('//')[1]; // e.g. foo.com/path/to/bar
      return domainAndPath.split('/')[0];
-    } 
+    }
     catch (err) {
       return null;
     }
-  }
+  } */
 
 
 }
